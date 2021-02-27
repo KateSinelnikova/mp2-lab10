189d0318030a79ae1b1ccc688d61a58f6d8d3675
Sinelnikova.13 <Sinelnikova.13@mail.ru>
2021-02-27 10:46:35 +0300

    lab10

---
+++ polinomlib/TPolinomial.h
@@ -0,0 +1,22 @@
+#ifndef POLINOMLIB_TPOLINOMIAL_H
+#define POLINOMLIB_TPOLINOMIAL_H
+
+#include "TMonom.h"
+#include "TList.h"
+#include <iostream>
+using namespace std;
+
+class TPolinomial : public TList<TMonomData> {
+public:
+    TPolinomial() = default;
+    TPolinomial(TPolinomial& polinomial);
+    ~TPolinomial() = default;
+
+    TPolinomial& operator+= (TMonom& _v);
+
+    TPolinomial operator* (TPolinomial& _v);
+    TPolinomial operator+ (TPolinomial& _v);
+    TPolinomial operator- (TPolinomial& _v);
+
+};
+#endif
